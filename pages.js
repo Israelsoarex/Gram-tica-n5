8e661e94e564aca192bac578dd400580547d4b76
Israelsoarex <122483799+Israelsoarex@users.noreply.github.com>
2023-01-11 16:48:55 -0300

    Add files via upload

---
+++ pages.js
@@ -0,0 +1,35 @@
+let pageList = [
+    {
+        name:"Particula は (wa) Marcador de tópico de",
+        path:"Particulaは.html",
+    },
+    {
+        name:"Partícula も (mo)",
+        path:"Particulaも.html",
+    },
+    {
+        name:"Partícula が (ga) Marcador de sujeito",
+        path:"Particulaが.html",
+    },
+    {
+        name:"Partícula で (de)",
+        path:"partículaで.html",
+    },
+    {
+        name:"Partícula と (to)",
+        path:"partículaと.html",
+    },
+    {
+        name:"Partícula へ (e)",
+        path:"partículaへ.html",
+    },
+    {
+        name:"Partícula を (o)",
+        path:"partículaを.html",
+    },
+];
+
+/* {
+        name:"",
+        path:"",
+    }, */
