8a988bec4821a64c07829afc06b1a5f24b3f5165
Israelsoarex <122483799+Israelsoarex@users.noreply.github.com>
2024-08-05 15:12:33 -0300

    Add files via upload

---
+++ recursos/kanjiDataN5.js
@@ -0,0 +1,731 @@
+let kanjiN5 = [
+    {
+        kanji: "一",
+        significado: "Um",
+        kunyomi: "ひと-",
+        onyomi: "イチ, イッ"
+    },
+    {
+        kanji: "二",
+        significado: "Dois",
+        kunyomi: "ふた-",
+        onyomi: "ニ"
+    },
+    {
+        kanji: "三",
+        significado: "Três",
+        kunyomi: "み-",
+        onyomi: "サン"
+    },
+    {
+        kanji: "四",
+        significado: "Quatro",
+        kunyomi: "よ-, よん",
+        onyomi: "シ"
+    },
+    {
+        kanji: "五",
+        significado: "Cinco",
+        kunyomi: "いつ-",
+        onyomi: "ゴ"
+    },
+    {
+        kanji: "六",
+        significado: "Seis",
+        kunyomi: "む-",
+        onyomi: "ロク"
+    },
+    {
+        kanji: "七",
+        significado: "Sete",
+        kunyomi: "なな-",
+        onyomi: "シチ"
+    },
+    {
+        kanji: "八",
+        significado: "Oito",
+        kunyomi: "や-",
+        onyomi: "ハチ"
+    },
+    {
+        kanji: "九",
+        significado: "Nove",
+        kunyomi: "ここの-",
+        onyomi: "キュウ"
+    },
+    {
+        kanji: "十",
+        significado: "Dez",
+        kunyomi: "とお-",
+        onyomi: "ジュウ"
+    },
+    {
+        kanji: "百",
+        significado: "Cem",
+        kunyomi: "もも",
+        onyomi: "ヒャク"
+    },
+    {
+        kanji: "千",
+        significado: "Mil",
+        kunyomi: "ち",
+        onyomi: "セン"
+    },
+    {
+        kanji: "万",
+        significado: "Dez Mil",
+        kunyomi: "よろず",
+        onyomi: "マン"
+    },
+    {
+        kanji: "億",
+        significado: "Cem Milhões",
+        kunyomi: "",
+        onyomi: "オク"
+    },
+    {
+        kanji: "日",
+        significado: "Dia, Sol",
+        kunyomi: "ひ, か",
+        onyomi: "ニチ, ジツ"
+    },
+    {
+        kanji: "週",
+        significado: "Semana",
+        kunyomi: "",
+        onyomi: "シュウ"
+    },
+    {
+        kanji: "月",
+        significado: "Mês, Lua",
+        kunyomi: "つき",
+        onyomi: "ゲツ, ガツ"
+    },
+    {
+        kanji: "年",
+        significado: "Ano",
+        kunyomi: "とし",
+        onyomi: "ネン"
+    },
+    {
+        kanji: "時",
+        significado: "Hora, Tempo",
+        kunyomi: "とき",
+        onyomi: "ジ"
+    },
+    {
+        kanji: "間",
+        significado: "Intervalo",
+        kunyomi: "あいだ, ま",
+        onyomi: "カン"
+    },
+    {
+        kanji: "分",
+        significado: "Minuto, Dividir",
+        kunyomi: "わ",
+        onyomi: "ブン, フン"
+    },
+    {
+        kanji: "午",
+        significado: "Meio-dia",
+        kunyomi: "ご",
+        onyomi: "ゴ"
+    },
+    {
+        kanji: "前",
+        significado: "Antes",
+        kunyomi: "まえ",
+        onyomi: "ゼン"
+    },
+    {
+        kanji: "後",
+        significado: "Depois",
+        kunyomi: "うし, あと",
+        onyomi: "ゴ, コウ"
+    },
+{
+    kanji: "今",
+    significado: "Agora",
+    kunyomi: "いま",
+    onyomi: "コン"
+},
+{
+    kanji: "先",
+    significado: "Antes",
+    kunyomi: "さき",
+    onyomi: "セン"
+},
+{
+    kanji: "来",
+    significado: "Vir",
+    kunyomi: "く,き",
+    onyomi: "ライ"
+},
+{
+    kanji: "毎",
+    significado: "Cada",
+    kunyomi: "ごと",
+    onyomi: "マイ"
+},
+{
+    kanji: "人",
+    significado: "Pessoa",
+    kunyomi: "ひと",
+    onyomi: "ジン, ニン"
+},
+{
+    kanji: "男",
+    significado: "Homem",
+    kunyomi: "おとこ",
+    onyomi: "ダン, ナン"
+},
+{
+    kanji: "女",
+    significado: "Mulher",
+    kunyomi: "おんな",
+    onyomi: "ジョ"
+},
+{
+    kanji: "子",
+    significado: "Criança",
+    kunyomi: "こ",
+    onyomi: "シ"
+},
+{
+    kanji: "母",
+    significado: "Mãe",
+    kunyomi: "はは",
+    onyomi: "ボ"
+},
+{
+    kanji: "父",
+    significado: "Pai",
+    kunyomi: "ちち",
+    onyomi: "フ"
+},
+{
+    kanji: "友",
+    significado: "Amigo",
+    kunyomi: "とも",
+    onyomi: "ユウ"
+},
+{
+    kanji: "本",
+    significado: "Livro",
+    kunyomi: "もと",
+    onyomi: "ホン"
+},
+{
+    kanji: "気",
+    significado: "Espírito",
+    kunyomi: "き",
+    onyomi: ""
+},
+{
+    kanji: "生",
+    significado: "Vida",
+    kunyomi: "い, う",
+    onyomi: "セイ, ショウ"
+},
+{
+    kanji: "車",
+    significado: "Carro",
+    kunyomi: "くるま",
+    onyomi: "シャ"
+},
+{
+    kanji: "語",
+    significado: "Idioma",
+    kunyomi: "かた",
+    onyomi: "ゴ"
+},
+{
+    kanji: "耳",
+    significado: "Orelha",
+    kunyomi: "みみ",
+    onyomi: "ジ"
+},
+{
+    kanji: "手",
+    significado: "Mão",
+    kunyomi: "て",
+    onyomi: "シュ"
+},
+{
+    kanji: "足",
+    significado: "Pé",
+    kunyomi: "あし, た",
+    onyomi: "ソク"
+},
+{
+    kanji: "目",
+    significado: "Olho",
+    kunyomi: "め",
+    onyomi: "モク"
+},
+{
+    kanji: "口",
+    significado: "Boca",
+    kunyomi: "くち",
+    onyomi: "コウ"
+},
+{
+    kanji: "店",
+    significado: "Loja",
+    kunyomi: "みせ",
+    onyomi: "テン"
+},
+{
+    kanji: "駅",
+    significado: "Estação",
+    kunyomi: "",
+    onyomi: "エキ"
+},
+{
+    kanji: "道",
+    significado: "Caminho",
+    kunyomi: "みち",
+    onyomi: "ドウ"
+},
+{
+    kanji: "国",
+    significado: "País",
+    kunyomi: "くに",
+    onyomi: "コク"
+},
+{
+    kanji: "学",
+    significado: "Estudo",
+    kunyomi: "まな",
+    onyomi: "ガク"
+},
+{
+    kanji: "校",
+    significado: "Escola",
+    kunyomi: "",
+    onyomi: "コウ"
+},
+{
+    kanji: "名",
+    significado: "Nome",
+    kunyomi: "な",
+    onyomi: "メイ, ミョウ"
+},
+{
+    kanji: "円",
+    significado: "Iene, Círculo",
+    kunyomi: "まる",
+    onyomi: "エン"
+},
+{
+    kanji: "半",
+    significado: "Meio",
+    kunyomi: "なか",
+    onyomi: "ハン"
+},
+{
+    kanji: "全",
+    significado: "Todo",
+    kunyomi: "すべて",
+    onyomi: "ゼン"
+},
+{
+    kanji: "何",
+    significado: "O quê",
+    kunyomi: "なに, なん",
+    onyomi: "カ"
+},
+{
+    kanji: "火",
+    significado: "Fogo",
+    kunyomi: "ひ",
+    onyomi: "カ"
+},
+{
+    kanji: "水",
+    significado: "Água",
+    kunyomi: "みず",
+    onyomi: "スイ"
+},
+{
+    kanji: "木",
+    significado: "Árvore",
+    kunyomi: "き",
+    onyomi: "モク"
+},
+{
+    kanji: "金",
+    significado: "Ouro, Dinheiro",
+    kunyomi: "かね",
+    onyomi: "キン"
+},
+{
+    kanji: "土",
+    significado: "Terra",
+    kunyomi: "つち",
+    onyomi: "ド"
+},
+{
+    kanji: "海",
+    significado: "Mar",
+    kunyomi: "うみ",
+    onyomi: "カイ"
+},
+{
+    kanji: "川",
+    significado: "Rio",
+    kunyomi: "かわ",
+    onyomi: "セン"
+},
+{
+    kanji: "山",
+    significado: "Montanha",
+    kunyomi: "やま",
+    onyomi: "サン"
+},
+{
+    kanji: "花",
+    significado: "Flor",
+    kunyomi: "はな",
+    onyomi: "カ"
+},
+{
+    kanji: "天",
+    significado: "Céu",
+    kunyomi: "あめ,あま",
+    onyomi: "テン"
+},
+{
+    kanji: "空",
+    significado: "Vazio, Céu",
+    kunyomi: "そら, から",
+    onyomi: "クウ"
+},
+{
+    kanji: "晴",
+    significado: "Ensolarado",
+    kunyomi: "は",
+    onyomi: "セイ"
+},
+{
+    kanji: "雨",
+    significado: "Chuva",
+    kunyomi: "あめ",
+    onyomi: "ウ"
+},
+{
+    kanji: "雪",
+    significado: "Neve",
+    kunyomi: "ゆき",
+    onyomi: "セツ"
+},
+{
+    kanji: "雲",
+    significado: "Nuvem",
+    kunyomi: "くも",
+    onyomi: "ウン"
+},
+{
+    kanji: "風",
+    significado: "Vento",
+    kunyomi: "かぜ",
+    onyomi: "フウ"
+},
+{
+    kanji: "電",
+    significado: "Eletricidade",
+    kunyomi: "",
+    onyomi: "デン"
+},
+{
+    kanji: "外",
+    significado: "Fora",
+    kunyomi: "そと",
+    onyomi: "ガイ"
+},
+{
+    kanji: "内",
+    significado: "Dentro",
+    kunyomi: "うち",
+    onyomi: "ナイ"
+},
+{
+    kanji: "上",
+    significado: "Cima",
+    kunyomi: "うえ, あ",
+    onyomi: "ジョウ"
+},
+{
+    kanji: "下",
+    significado: "Baixo",
+    kunyomi: "した, く",
+    onyomi: "カ, ゲ"
+},
+{
+    kanji: "右",
+    significado: "Direita",
+    kunyomi: "みぎ",
+    onyomi: "ユウ"
+},
+{
+    kanji: "左",
+    significado: "Esquerda",
+    kunyomi: "ひだり",
+    onyomi: "サ"
+},
+{
+    kanji: "中",
+    significado: "Meio, Dentro",
+    kunyomi: "なか",
+    onyomi: "チュウ"
+},
+{
+    kanji: "北",
+    significado: "Norte",
+    kunyomi: "きた",
+    onyomi: "ホク"
+},
+{
+    kanji: "西",
+    significado: "Oeste",
+    kunyomi: "にし",
+    onyomi: "セイ, サイ"
+},
+{
+    kanji: "東",
+    significado: "Leste",
+    kunyomi: "ひがし",
+    onyomi: "トウ"
+},
+{
+    kanji: "南",
+    significado: "Sul",
+    kunyomi: "みなみ",
+    onyomi: "ナン"
+},
+{
+    kanji: "見",
+    significado: "Ver",
+    kunyomi: "みる",
+    onyomi: "ケン"
+},
+{
+    kanji: "聞",
+    significado: "Ouvir",
+    kunyomi: "き",
+    onyomi: "ブン, モン"
+},
+{
+    kanji: "書",
+    significado: "Escrever",
+    kunyomi: "か",
+    onyomi: "ショ"
+},
+{
+    kanji: "言",
+    significado: "Falar",
+    kunyomi: "いう",
+    onyomi: "ゲン"
+},
+{
+    kanji: "話",
+    significado: "Conversar",
+    kunyomi: "はな, はなし",
+    onyomi: "ワ"
+},
+{
+    kanji: "読",
+    significado: "Ler",
+    kunyomi: "よ",
+    onyomi: "ドク"
+},
+{
+    kanji: "行",
+    significado: "Ir",
+    kunyomi: "い, おこな",
+    onyomi: "コウ"
+},
+{
+    kanji: "買",
+    significado: "Comprar",
+    kunyomi: "か",
+    onyomi: "バイ"
+},
+{
+    kanji: "出",
+    significado: "Sair",
+    kunyomi: "で, だ",
+    onyomi: "シュツ"
+},
+{
+    kanji: "入",
+    significado: "Entrar",
+    kunyomi: "はい, い",
+    onyomi: "ニュウ"
+},
+{
+    kanji: "食",
+    significado: "Comer",
+    kunyomi: "たべ",
+    onyomi: "ショク"
+},
+{
+    kanji: "飲",
+    significado: "Beber",
+    kunyomi: "の",
+    onyomi: "イン"
+},
+{
+    kanji: "休",
+    significado: "Descansar",
+    kunyomi: "やす",
+    onyomi: "キュウ"
+},
+{
+    kanji: "会",
+    significado: "Encontrar",
+    kunyomi: "あ",
+    onyomi: "カイ"
+},
+{
+    kanji: "多",
+    significado: "Muito",
+    kunyomi: "おお",
+    onyomi: "タ"
+},
+{
+    kanji: "少",
+    significado: "Pouco",
+    kunyomi: "すく, すこし",
+    onyomi: "ショウ"
+},
+{
+    kanji: "古",
+    significado: "Antigo",
+    kunyomi: "ふる",
+    onyomi: "コ"
+},
+{
+    kanji: "新",
+    significado: "Novo",
+    kunyomi: "あたらし",
+    onyomi: "シン"
+},
+{
+    kanji: "大",
+    significado: "Grande",
+    kunyomi: "おお",
+    onyomi: "ダイ, タイ"
+},
+{
+    kanji: "小",
+    significado: "Pequeno",
+    kunyomi: "ちい",
+    onyomi: "ショウ"
+},
+{
+    kanji: "長",
+    significado: "Longo",
+    kunyomi: "なが",
+    onyomi: "チョウ"
+},
+{
+    kanji: "短",
+    significado: "Curto",
+    kunyomi: "みじか",
+    onyomi: "タン"
+},
+{
+    kanji: "遠",
+    significado: "Distante",
+    kunyomi: "とお",
+    onyomi: "エン"
+},
+{
+    kanji: "近",
+    significado: "Perto",
+    kunyomi: "ちか",
+    onyomi: "キン"
+},
+{
+    kanji: "白",
+    significado: "Branco",
+    kunyomi: "しろ",
+    onyomi: "ハク"
+},
+{
+    kanji: "黒",
+    significado: "Preto",
+    kunyomi: "くろ",
+    onyomi: "コク"
+},
+{
+    kanji: "高",
+    significado: "Alto",
+    kunyomi: "たか",
+    onyomi: "コウ"
+},
+{
+    kanji: "安",
+    significado: "Barato, Seguro",
+    kunyomi: "やす",
+    onyomi: "アン"
+}
+];
+
+/*
+    {
+        kanji: "",
+        significado: "",
+        kunyomi: "",
+        onyomi: "",
+        vocab: ""
+    },
+*/
+
+function criarTabela(kanjiArray) {
+    let table = document.createElement('table');
+    let thead = document.createElement('thead');
+    let tbody = document.createElement('tbody');
+    let caption = document.createElement('caption');
+    caption.textContent = 'Tabela dos Kanji N5';
+    table.appendChild(caption);
+    // Criar o cabeçalho da tabela
+    let headerRow = document.createElement('tr');
+    let headers = ['Kanji', 'Sig', 'Kun', 'On'];
+    
+    headers.forEach(headerText => {
+        let th = document.createElement('th');
+        th.textContent = headerText;
+        headerRow.appendChild(th);
+    });
+    
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+    let i = 0;
+    // Adicionar as linhas de dados
+    kanjiArray.forEach(item => {
+        i++
+        
+        let row = document.createElement('tr');
+        
+        Object.values(item).forEach(value => {
+            let td = document.createElement('td');
+            td.textContent = value;
+            row.appendChild(td);
+        });
+        
+        tbody.appendChild(row);
+    });
+    
+    table.appendChild(tbody);
+    console.log(i)
+    return table;
+}
+
+// Selecionar o div onde a tabela será adicionada
+let mainKanji = document.querySelector("#mainKanji");
+
+// Criar a tabela e adicioná-la ao div
+let tabela = criarTabela(kanjiN5);
+mainKanji.appendChild(tabela);
